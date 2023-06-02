037fa8cc2b3f6a034af4e659260f42109eccd381
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2023-06-02 17:45:52 +0200

    restructuring the project (again)

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,15 @@
 module Main (main) where
     -- main function
+
+import Syntax.Lex
+import Syntax.Par
+import Syntax.Print
+import Syntax.Abs
+import Semantics
+
+
+import Syntax.ErrM
+  
 main :: IO ()
 main = do
-  print "Hallo"
+  print "To be implemented"
