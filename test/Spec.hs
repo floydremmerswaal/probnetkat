86a88a4bb870a8b2b554fd60732651b0fd038754
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2023-06-27 14:12:11 +0200

    add new test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,14 +5,23 @@ import Semantics
 import Syntax.Par (myLexer, pExp)
 import Control.Monad.Bayes.Enumerator
 import Control.Arrow
+import qualified Data.Set as Set
+
 
 stringToExp :: String -> Exp
 stringToExp s = case pExp (myLexer s) of
     Left _ -> ESkip
     Right tree -> tree
 
-testTransExp :: Syntax.Abs.Exp -> Kleisli Enumerator SH SH
-testTransExp = transExp
+
+-- not used/working yet
+testTransExp :: Syntax.Abs.Exp -> [(SH, Double)]
+testTransExp tree = do
+    let history = [[]] :: History
+    let initialSet = Set.fromList [history] :: SH
+    let kleisliArrow = transExp tree :: Kleisli Enumerator SH SH
+    let result = runKleisli kleisliArrow initialSet
+    enumerator result
 
 main :: IO ()
 main = hspec $ do
@@ -20,7 +29,9 @@ main = hspec $ do
         it "can parse assignment" $ do
             stringToExp "x <- 1" `shouldBe` EAss (Ident "x") 1
         it "can parse test" $ do
-            stringToExp "x = 1" `shouldBe` ETest (Ident "x") 1
+            stringToExp "x = 1" `shouldBe` EEq (Ident "x") 1
+        it "can parse negative test" $ do
+            stringToExp "x != 1" `shouldBe` ENeq (Ident "x") 1
         it "can parse dup" $ do
             stringToExp "dup" `shouldBe` EDup
         it "can parse skip" $ do
@@ -32,8 +43,4 @@ main = hspec $ do
         it "can parse prob" $ do
             stringToExp "x <- 1 +[0.5] y <- 2" `shouldBe` EProb (EAss (Ident "x") 1) 0.5 (EAss (Ident "y") 2)
         it "can parse par" $ do
-            stringToExp "x <- 1 & y <- 2" `shouldBe` Epar (EAss (Ident "x") 1) (EAss (Ident "y") 2)
-    -- TODO: testing the transformation    
-    -- describe "Probnetkat transformation" $ do
-    --         it "can transform assignment" $ do
-    --             testTransExp (stringToExp "x <- 1") `shouldBe` assign (Field (Ident "x") 1)
+            stringToExp "x <- 1 & y <- 2" `shouldBe` Epar (EAss (Ident "x") 1) (EAss (Ident "y") 2)   
